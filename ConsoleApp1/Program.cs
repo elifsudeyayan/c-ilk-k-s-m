d8a164bc24638b658e834908e583356e1c6f4929
Elif Sude Yayan <elifsuyn00@gmail.com>
2023-08-17 15:01:41 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿ 
+Console.WriteLine("Hello, World!");
+
+//todo: iki tane değişken olsun ve bu iki değişkeni toplasın ekrana bastırsın.
+
+//daire çevresini ve alananı heaplayan kodlar  
+// alan : pi* r^2 
+// çevre: 2*pi*r
+
+double r = 8;
+double pi = 3.14;
+double çevre = 2 * pi * r;
+double alan = pi * (r*r);
+Console.WriteLine($"dairenin alani ={alan}");
+Console.WriteLine($"dairenin cevre = {çevre}");
+    
+
+
+
+
+
+
+
